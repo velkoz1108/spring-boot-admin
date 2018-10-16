b1f9d29b29c370c310ca7b203b32060568cb759e
wangtao <twin@jollycorp.com>
2018-10-16 17:20:24 +0800

    add email notify in admin server

--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 引用方式  `@project.name@` ，在application.yml需要加单引号或双引号， `'@project.name@'`
 
 如果使用main方法启动程序则无法替换变量，显示为 @project.name@
+
 ![图片名称](start.png)
 
 可行方法如下：
@@ -12,8 +13,12 @@
 
 ####  spring-boot  run 
 * 双击spring-boot的maven插件run即可
+
 ![图片名称](run.png)
 
 
 
+##### 当App注册到服务中心（如Eureka、Consul、Zookeeper等），在App中无需Spring-Boot-Admin的依赖，Spring-Admin-Boot可以通过服务中心自动实现监控
+
+![图片名称](auto.png)
 
--- admin-server/src/test/java/com/wangtao/adminserver/AdminServerApplicationTests.java
+++ admin-server/src/test/java/com/wangtao/adminserver/AdminServerApplicationTests.java
@@ -2,7 +2,11 @@ package com.wangtao.adminserver;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
@@ -11,6 +15,26 @@ public class AdminServerApplicationTests {
 
     @Test
     public void contextLoads() {
+    }
+
+    @Autowired
+    private JavaMailSender mailSender; //自动注入的Bean
+
+    @Value("${spring.mail.username}")
+    private String sender; //读取配置文件中的参数
+
+    @Value("${spring.boot.admin.notify.mail.to}")
+    private String to; //读取配置文件中的参数
+
+
+    @Test
+    public void sendSimpleMail() throws Exception {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom(sender);
+        message.setTo(to);
+        message.setSubject("主题：简单邮件");
+        message.setText("测试邮件内容");
+        mailSender.send(message);
     }
 
 }
