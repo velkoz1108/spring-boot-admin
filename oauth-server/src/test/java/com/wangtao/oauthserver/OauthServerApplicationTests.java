0ed43857afb58fa290cc4d2ba041fd1ffec0b613
wangtao <twin@jollycorp.com>
2018-11-22 19:21:16 +0800

    test demo

--- oauth-server/src/test/java/com/wangtao/oauthserver/OauthServerApplicationTests.java
+++ oauth-server/src/test/java/com/wangtao/oauthserver/OauthServerApplicationTests.java
@@ -3,6 +3,7 @@ package com.wangtao.oauthserver;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
@@ -11,6 +12,8 @@ public class OauthServerApplicationTests {
 
     @Test
     public void contextLoads() {
+        String finalSecret = new BCryptPasswordEncoder().encode("123456"); // $2a$10$cOf2Mzy7Nr.kVsdP13HIyewvY7DviOorG/j73IKvkQ50Vo6aYptTy
+        System.out.println("finalSecret = " + finalSecret);
     }
 
 }
