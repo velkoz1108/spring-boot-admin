2cb5ed0649c260dfa63fe91d4af99f34c528071e
wangtao <twin@jollycorp.com>
2018-11-23 16:34:01 +0800

    add some files

---
+++ oauth-server-code/src/test/java/com/wangtao/oauthservercode/OauthServerCodeApplicationTests.java
@@ -0,0 +1,16 @@
+package com.wangtao.oauthservercode;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OauthServerCodeApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
