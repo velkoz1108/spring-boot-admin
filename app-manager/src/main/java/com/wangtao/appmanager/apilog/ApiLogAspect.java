fac6798dd4c60a63c5bb64d7dfefeb1a3e190b7e
wangtao <twin@jollycorp.com>
2018-11-23 13:19:37 +0800

    save api log by aop

---
+++ app-manager/src/main/java/com/wangtao/appmanager/apilog/ApiLogAspect.java
@@ -0,0 +1,75 @@
+package com.wangtao.appmanager.apilog;
+
+import com.alibaba.fastjson.JSON;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Date;
+
+/**
+ * @author : wangtao
+ * @date : 2018/11/23 9:42  星期五
+ */
+@Slf4j
+@Aspect
+@Component
+public class ApiLogAspect {
+    @Autowired
+    private ApiLogService apiLogService;
+
+    @Pointcut("execution(* com.wangtao.appmanager.web.*.*(..))")
+    private void controllerAspect() {
+    }
+
+    /**
+     * @param pjp
+     * @return
+     * @throws Throwable
+     */
+    @Around("controllerAspect()")
+    public Object around(ProceedingJoinPoint pjp) throws Throwable {
+        long start = System.currentTimeMillis();
+        Object object = pjp.proceed();
+        long usedTime = System.currentTimeMillis() - start;
+        //记录日志
+        saveApiLog(pjp, usedTime);
+        return object;
+    }
+
+    private void saveApiLog(ProceedingJoinPoint pjp, long usedTime) {
+        try {
+            HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
+            log.info("URI : " + JSON.toJSONString(request.getRequestURI()));
+
+            String remote = IpAddressUtil.getIP(request);
+            log.info("remote : " + remote);
+
+            String ip = IpAddressUtil.getIpAddress();
+            log.info("localip : " + ip);
+
+            Object target = pjp.getTarget();
+            log.info("target: " + target.toString());
+
+            String methodName = pjp.getSignature().getName();
+            log.info("method : " + methodName);
+
+            Object[] args = pjp.getArgs();
+            log.info("args : " + JSON.toJSONString(args));
+
+            Date date = new Date();
+            log.info("time : " + date.toString());
+
+            apiLogService.saveApiLog();
+        } catch (Exception e) {
+            log.info("SaveApiLog Exception", e);
+        }
+    }
+}
---
+++ app-manager/src/main/java/com/wangtao/appmanager/apilog/IpAddressUtil.java
@@ -0,0 +1,75 @@
+package com.wangtao.appmanager.apilog;
+
+
+import lombok.extern.slf4j.Slf4j;
+
+import javax.servlet.http.HttpServletRequest;
+import java.net.Inet4Address;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.util.Enumeration;
+
+@Slf4j
+public class IpAddressUtil {
+
+
+    /**
+     * 获取本机ip地址 非127.0.0.1
+     *
+     * @return IP地址
+     */
+    public static String getIpAddress() {
+        try {
+            Enumeration<NetworkInterface> networkInterfaces = NetworkInterface.getNetworkInterfaces();
+            while (networkInterfaces.hasMoreElements()) {
+                NetworkInterface networkInterface = networkInterfaces.nextElement();
+                Enumeration<InetAddress> inetAddresses = networkInterface.getInetAddresses();
+                while (inetAddresses.hasMoreElements()) {
+                    InetAddress inetAddress = inetAddresses.nextElement();
+                    if (inetAddress instanceof Inet4Address
+                            && !inetAddress.isLoopbackAddress()) {
+                        return inetAddress.getHostAddress();
+                    }
+                }
+            }
+        } catch (SocketException e) {
+            log.error(e.getMessage(), e);
+        }
+        return "";
+    }
+
+    public static String getIP(HttpServletRequest request) {
+        String ip = request.getHeader("x-forwarded-for");
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("Proxy-Client-IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("WL-Proxy-Client-IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("X-Real-IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("HTTP_CLIENT_IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("HTTP_X_FORWARDED_FOR");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getRemoteAddr();
+        }
+
+        if (ip.length() > 15) {
+            String[] ips = ip.split(",");
+            for (int index = 0; index < ips.length; index++) {
+                String strIp = (String) ips[index];
+                if (!("unknown".equalsIgnoreCase(strIp))) {
+                    ip = strIp;
+                    break;
+                }
+            }
+        }
+        return ip;
+    }
+}
