45814f45919a92b94de831973272f3677eaaa2a8
wangtao <twin@jollycorp.com>
2018-11-27 10:53:59 +0800

    第三方登录可以，自己的登录没有搞好

---
+++ oauth-client/src/main/java/com/wangtao/oauthclient/web/LoginController.java
@@ -0,0 +1,21 @@
+package com.wangtao.oauthclient.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.annotation.security.PermitAll;
+
+/**
+ * @author : wangtao
+ * @date : 2018/11/26 20:45  星期一
+ */
+
+@Controller
+public class LoginController {
+
+//    @PermitAll
+//    @GetMapping("/login")
+//    public String login() {
+//        return "login";
+//    }
+}
--- oauth-client/src/main/java/com/wangtao/oauthclient/web/UserController.java
+++ oauth-client/src/main/java/com/wangtao/oauthclient/web/UserController.java
@@ -1,5 +1,9 @@
 package com.wangtao.oauthclient.web;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,6 +14,8 @@ import java.util.Map;
 /**
  * @author : wangtao
  * @date : 2018/11/21 15:26  星期三
+ * <p>
+ * 获取用户信息的多种方法
  */
 
 @RestController
@@ -25,6 +31,27 @@ public class UserController {
         Map<String, String> map = new LinkedHashMap<>();
         map.put("name", principal.getName());
         return map;
+    }
+
+    @RequestMapping("/me/authentication")
+    public Authentication authentication() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        return authentication;
+    }
+
+    @RequestMapping("/me/authentication2")
+    public Authentication authentication2(Authentication authentication) {
+        return authentication;
+    }
+
+    /**
+     * 返回null了？
+     * @param userDetails
+     * @return
+     */
+    @RequestMapping("/me/userDetails")
+    public UserDetails getUserDetails(@AuthenticationPrincipal UserDetails userDetails) {
+        return userDetails;
     }
 }
 
