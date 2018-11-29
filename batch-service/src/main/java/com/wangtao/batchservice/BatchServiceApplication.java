bd778995010c6f169dfb4e0fc98e5a1ccaf63224
wangtao <twin@jollycorp.com>
2018-11-29 15:49:54 +0800

    batch server demo

--- batch-service/src/main/java/com/wangtao/batchservice/BatchServiceApplication.java
+++ batch-service/src/main/java/com/wangtao/batchservice/BatchServiceApplication.java
@@ -3,10 +3,17 @@ package com.wangtao.batchservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author WIN7
+ * <p>
+ * 参考文档：
+ * https://spring.io/guides/gs/batch-processing/
+ * https://www.cnblogs.com/Leo_wl/p/6139576.html
+ */
 @SpringBootApplication
 public class BatchServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BatchServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BatchServiceApplication.class, args);
+    }
 }
