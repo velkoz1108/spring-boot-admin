b702f13b97bf63fbd21f8b0555838a1b136d1e28
taoshen182 <1227602328@qq.com>
2018-12-23 16:59:26 +0800

    gateway demo 2

--- gateway/src/main/java/com/wangtao/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/wangtao/gateway/GatewayApplication.java
@@ -2,15 +2,12 @@ package com.wangtao.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.gateway.filter.factory.StripPrefixGatewayFilterFactory;
-import org.springframework.cloud.gateway.route.RouteLocator;
-import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
-import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RestController;
 
 // tag::code[]
 
 //@EnableConfigurationProperties(UriConfiguration.class)
-//@RestController
+@RestController
 @SpringBootApplication
 public class GatewayApplication {
     // tag::fallback[]
@@ -25,32 +22,37 @@ public class GatewayApplication {
         SpringApplication.run(GatewayApplication.class, args);
     }
 
+//    @Bean
+//    public RouteLocator myRoutes(RouteLocatorBuilder builder) {
+//        return builder.routes().build();
+//    }
 
-    @Bean
-    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {//, UriConfiguration uriConfiguration
-//        String httpUri = uriConfiguration.getHttpbin();
-        StripPrefixGatewayFilterFactory.Config config = new StripPrefixGatewayFilterFactory.Config();
-        config.setParts(1);
-        return builder.routes()
-                .route("host_route",
-                        r -> r.path("/a/**").filters(f -> f.stripPrefix(1)).uri("http://localhost:8081"))
-
-                .route("host_route",
-                        r -> r.path("/b/**").filters(f -> f.stripPrefix(1)).uri("http://localhost:8083"))
 //
-//                .route(p -> p
-//                        .path("/get")
-//                        .filters(f -> f.addRequestHeader("Hello", "World"))
-//                        .uri("http://httpbin.org:80"))
+//    @Bean
+//    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {//, UriConfiguration uriConfiguration
+////        String httpUri = uriConfiguration.getHttpbin();
+//        StripPrefixGatewayFilterFactory.Config config = new StripPrefixGatewayFilterFactory.Config();
+//        config.setParts(1);
+//        return builder.routes()
+//                .route("host_route",
+//                        r -> r.path("/a/**").filters(f -> f.stripPrefix(1)).uri("http://localhost:8081"))
 //
-//                .route(p -> p
-//                        .host("*.hystrix.com")
-//                        .filters(f -> f.hystrix(config -> config.setName("mycmd")))
-//                        .uri("http://httpbin.org:80"))
-
-
-                .build();
-    }
+//                .route("host_route",
+//                        r -> r.path("/b/**").filters(f -> f.stripPrefix(1)).uri("http://localhost:8083"))
+////
+////                .route(p -> p
+////                        .path("/get")
+////                        .filters(f -> f.addRequestHeader("Hello", "World"))
+////                        .uri("http://httpbin.org:80"))
+////
+////                .route(p -> p
+////                        .host("*.hystrix.com")
+////                        .filters(f -> f.hystrix(config -> config.setName("mycmd")))
+////                        .uri("http://httpbin.org:80"))
+//
+//
+//                .build();
+//    }
 }
 
 //@ConfigurationProperties
